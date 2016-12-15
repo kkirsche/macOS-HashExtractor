852f56a8531fdf4cb7c89d418981be61f6de3920
Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
2016-12-15 16:02:39 -0500

    Make it more method oriented

--- extractor.py
+++ extractor.py
@@ -7,7 +7,26 @@ import fnmatch
 import xml.etree.ElementTree as ET
 import string
 
-basepath = '/var/db/dslocal/nodes/Default/users/'
+
+def get_user_plist_filenames():
+    files = []
+    for filename in os.listdir(basepath):
+        if fnmatch.fnmatch(filename, '[!_|!nobody]*.plist'):
+            files.append(filename)
+
+    return files
+
+
+def get_plist_contents_from(filename):
+    path = basepath + filename
+    result = subprocess.run([
+        u"sudo /usr/bin/defaults read {}".format(path) +
+        u" ShadowHashData 2>/dev/null|tr -dc 0-9a-f|xxd -r -p|" +
+        u"plutil -convert xml1 - -o -"
+    ], universal_newlines=True, shell=True,
+       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    return result.stdout
 
 
 def remove_whitespace(hash_str):
@@ -15,29 +34,50 @@ def remove_whitespace(hash_str):
         ord(x): '' for x in set(string.whitespace)
     })
 
-for filename in os.listdir(basepath):
-    if fnmatch.fnmatch(filename, '[!_|!nobody]*.plist'):
-        path = basepath + filename
-        result = subprocess.run([
-            u"sudo /usr/bin/defaults read {}".format(path) +
-            u" ShadowHashData 2>/dev/null|tr -dc 0-9a-f|xxd -r -p|" +
-            u"plutil -convert xml1 - -o -"
-        ], universal_newlines=True, shell=True,
-           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+def parse_plist(plist_str):
+    root = ET.fromstring(plist_str)
+    for child in root.findall(".//data[1]"):
+        entropy = child.text.replace(" ", "").strip()
+    for child in root.findall(".//integer[1]"):
+        iterations = child.text.strip()
+    for child in root.findall(".//data[2]"):
+        salt = child.text.strip()
+
+    return {
+        "entropy": entropy,
+        "iterations": iterations,
+        "salt": salt
+    }
+
+
+def format_hash(hash_components):
+    hash_str = remove_whitespace(
+        u"$ml$" +
+        hash_components["iterations"] +
+        u"$" +
+        hash_components["salt"] +
+        u"$" +
+        hash_components["entropy"]
+    )
+    return hash_str
+
+
+def make_crypt_format(user, hash_str):
+    fmtd = "{}:{}".format(user, hash_str)
+    return fmtd
+
+
+if __name__ == '__main__':
+    basepath = '/var/db/dslocal/nodes/Default/users/'
+    files = get_user_plist_filenames()
+    for filename in files:
         user = filename.split('.')[0]
+        plist_contents = get_plist_contents_from(filename)
         try:
-            root = ET.fromstring(result.stdout)
-            for child in root.findall(".//data[1]"):
-                entropy = child.text.replace(" ", "").strip()
-            for child in root.findall(".//integer[1]"):
-                iterations = child.text.strip()
-            for child in root.findall(".//data[2]"):
-                salt = child.text.strip()
-            print(
-                u"{}:".format(user) +
-                remove_whitespace(u"$ml$"+iterations+u"$"+salt+u"$"+entropy)
-            )
+            hash_components = parse_plist(plist_contents)
+            formatted_hash = format_hash(hash_components)
+            print(make_crypt_format(user, formatted_hash))
             print()
         except:
             print(u"Oops! Something went wrong trying to extract" +
